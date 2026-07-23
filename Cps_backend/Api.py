56993d9a823599bfa6c76af5b082dfcb29a8cec3
wisdomandwealthteam-blip <wisdomandwealthteam@gmail.com>
2026-07-23 11:12:21 -0400

    Delete Cps_backend/Api.py

--- Cps_backend/Api.py
+++
@@ -1,8 +0,0 @@
-from .registry import REGISTRY
-
-def evaluate_contract(vertical, contract):
-    if vertical not in REGISTRY:
-        raise ValueError(f"Unknown vertical: {vertical}")
-
-    evaluator = REGISTRY[vertical]
-    return evaluator(contract)
